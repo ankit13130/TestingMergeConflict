3ed8b427e6bbfd28a70085861d278091658b479b
ankit13130 <guptaankit13130@gmail.com>
2024-04-01 15:27:06 +0530

    Add project files.

---
+++ TestingMergeConflict/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestingMergeConflict.Controllers;
+
+[Route("[Controller]")]
+[ApiController]
+public class EmployeeController : ControllerBase
+{
+    private readonly List<string> employeeName =
+    [
+        "Ankit",
+        "Naitik",
+        "Ram",
+        "Ganesh"
+    ];
+    //[HttpGet]
+    //public IActionResult GetAllEmployee()
+    //{
+    //    return Ok(employeeName);
+    //}
+}
